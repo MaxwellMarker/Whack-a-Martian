b0a4011acba0a4f878529da745f5df76f1088e75
Max Marker <maxmarker98@gmail.com>
2020-07-31 23:44:44 -0400

    basic target popup and scoring works

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,5 @@
 let scoreCount = 0;
+const targetIds = ['r1t1','r1t2','r1t3'];
 $(()=>{
     const clickTarget = (event) => {
         $(event.currentTarget).off('click', clickTarget);
@@ -12,3 +13,13 @@ $(()=>{
     } 
     $clickTarget()
 })
+const randomNumber = (maxNum) =>{
+
+}
+const revealTarget = () => {
+    const idNum = (Math.floor(Math.random()*3));
+    $(`#${targetIds[idNum]}`).css('transform', 'translate(0, 0)')
+    setTimeout(() => {$(`#${targetIds[idNum]}`).css('transform', 'translate(0, 125px)')}, 1200);
+}
+
+setInterval(revealTarget, 4000);
