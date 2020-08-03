3ef1111f3ba91139af37bd15ba42444f84def49e
Max Marker <maxmarker98@gmail.com>
2020-08-03 00:59:05 -0400

    condensed revealTarget functions and created data structure for type2 targets

--- javascript/app.js
+++ javascript/app.js
@@ -41,6 +41,38 @@ const targetIdsType1 = [
         endPosition: 'translate(-25px, -40px) rotate(-25deg)'
     }
 ];
+const targetIdsType2 = [
+    type2num1 = {
+        id: 'type2num1',
+        startPosition: 'translate(-20px, 100px) rotate(8deg)',
+        endPosition: 'translate(0, 0) rotate(8deg)'
+    },
+    type2num2 = {
+        id: 'type2num2',
+        startPosition: 'translate(20px, 100px) rotate(-8deg)',
+        endPosition: 'translate(0, 0) rotate(-8deg)'
+    },
+    type2num3 = {
+        id: 'type2num3',
+        startPosition: 'translate(-10px, 0) rotate(35deg)',
+        endPosition: 'translate(50px, -90px) rotate(35deg)'
+    },
+    type2num4 = {
+        id: 'type2num4',
+        startPosition: 'translate(-20px, 100px) rotate(8deg)',
+        endPosition: 'translate(0, 0) rotate(8deg)'
+    },
+    type2num5 = {
+        id: 'type2num5',
+        startPosition: 'translate(20px, 100px) rotate(-8deg)',
+        endPosition: 'translate(0, 0) rotate(-8deg)'
+    },
+    type2num6 = {
+        id: 'type2num6',
+        startPosition: 'translate(10px, 0px) rotate(-35deg)',
+        endPosition: 'translate(-50px, -90px) rotate(-35deg)'
+    }
+]
 $(() => {
     const clickTarget = (event) => {
         const targ = $(event.currentTarget);
@@ -58,13 +90,12 @@ $(() => {
     }
     $clickTarget()
 })
-
-const revealTarget1 = () => {
-    const target = targetIdsType1[(Math.floor(Math.random() * targetIdsType1.length))];
+const revealTarget = (targetArray, timeout) => {
+    const target = targetArray[(Math.floor(Math.random() * targetArray.length))];
     $(`#${target.id}`).css('transform', target.endPosition)
     setTimeout(() => {
         $(`#${target.id}`).css('transform', target.startPosition)
-    }, 1400);
+    }, timeout);
 }
-
-setInterval(revealTarget1, 2000);
+setInterval(function(){revealTarget(targetIdsType1, 1600);}, 3000);
+setInterval(function(){revealTarget(targetIdsType2, 1000);}, 4000);
